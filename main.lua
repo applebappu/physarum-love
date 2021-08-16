9f9f66b2962e608f5a51f2f3f20e98cc07de7216
Jo Anne Wilson <jo.a.wilson@protonmail.com>
2021-08-16 15:32:55 -0800

    infinite expansion with nutrients

--- main.lua
+++ main.lua
@@ -38,15 +38,15 @@ love.graphics.setFont(font)
 physarum = {
 	body_table = {},
 	starting_position = {
-		x = math.random(10,20),
-		y = math.random(10,20)
+		x = math.random(15, 20),
+		y = math.random(15, 20)
 	},
-	energy = 10
+	energy = 50
 }
 
 food = {
 	location_table = {},
-	nutrition = 5
+	global_nutrition = 50
 }
 
 map = {
@@ -70,9 +70,10 @@ map = {
 				if i == 1 or j == 1 or i == map.board_size.x or j == map.board_size.y then
 					map.map_table[i][j] = "#"
 				end
-
+				
+				local nutrition = food.global_nutrition
 				if map.map_table[i][j] == "f" then
-					food.location_table[i][j] = "%"
+					food.location_table[i][j] = {"%", nutrition}
 					map.map_table[i][j] = "."
 				end
 			end
@@ -86,6 +87,8 @@ directions = {
 	{1, -1},  {0 , 1}, {1, 1}
 }
 
+global_timer = 0
+
 InitMatrix = function(matrix)
 	local m = matrix
 	for i = 1, map.board_size.x do
@@ -96,41 +99,41 @@ InitMatrix = function(matrix)
 	end
 end
 
-CheckSurroundings = function(test_char)
-	local test_table = {}
-	local results_table = {}
-	
-	if test_char == "#" then
-		test_table = map.map_table
-	elseif test_char == "%" then
-		test_table = food.location_table
-	elseif test_char == "p" then
-		test_table = physarum.body_table
-	end
-
-	for i = 1, map.board_size.x do
-		for j = 1, map.board_size.y do
+ExpandPlasmodium = function()
+	for i = 2, (map.board_size.x - 1) do
+		for j = 2, (map.board_size.y - 1) do
 			if physarum.body_table[i][j] == "p" then
 				for n = 1, #directions do
 					local target_x = directions[n][1]
 					local target_y = directions[n][2]
+					local a = i + target_x
+					local b = j + target_y
 
-					if test_table[i + target_x][j + target_y] ~= test_char then
-						results_table[n] = false
-					else
-						results_table[n] = true
+					if map.map_table[a][b] == "." and physarum.energy > 0 and physarum.body_table[a][b] ~= "p" then
+						physarum.body_table[a][b] = "p"
+						physarum.energy = physarum.energy - 1
 					end
 				end
+				global_timer = 0
 			end
 		end
 	end
-	return results_table
 end
 
-SpreadPlasmodium = function()
-	-- check surroundings for walls, food, and body parts.
-	-- spread with a high priority onto food, then empty spaces, not onto walls or body
-	CheckSurroundings("#")
+ProcessNutrients = function()
+	for i = 2, (map.board_size.x - 1) do
+		for j = 2, (map.board_size.y - 1) do
+			if food.location_table[i][j][1] == "%" and physarum.body_table[i][j] == "p" then
+				physarum.energy = physarum.energy + 1
+				food.location_table[i][j][2] = food.location_table[i][j][2] - 1
+				global_timer = 0
+			end
+		end
+	end
+end
+
+ConsolidatePlasmodium = function()
+	
 end
 
 -- MAIN CODE --
@@ -143,20 +146,19 @@ map.RandomMap()
 physarum.body_table[physarum.starting_position.x][physarum.starting_position.y] = "p"
 map.map_table[physarum.starting_position.x][physarum.starting_position.y] = "."
 
-results = CheckSurroundings("#")
-for k,v in ipairs(results) do
-	print(k, v)
-end
-
 function love.draw()
 	for i = 1, map.board_size.x do
 		for j = 1, map.board_size.y do
 			love.graphics.setColor(255,255,255)
 			love.graphics.print(map.map_table[i][j], i * map.tile_size, j * map.tile_size)
-			if food.location_table[i][j] == "%" then
+			if food.location_table[i][j][1] == "%" then
 				love.graphics.setColor(0,0,0)
 				love.graphics.rectangle("fill",  i * map.tile_size,  j * map.tile_size, map.tile_size, map.tile_size)
-				love.graphics.setColor(218/255,165/255,32/255)
+				if physarum.body_table[i][j] == "p" then
+					love.graphics.setColor(255/255, 255/255, 0/255)
+				else
+					love.graphics.setColor(218/255,165/255,32/255)
+				end
 				love.graphics.print("%", i * map.tile_size, j * map.tile_size)
 			elseif physarum.body_table[i][j] == "p" then
 				love.graphics.setColor(0,0,0)
@@ -169,7 +171,11 @@ function love.draw()
 end
 
 function love.update(dt)
-
+	if global_timer >= 1 then
+		ExpandPlasmodium()
+		ProcessNutrients()
+	end
+	global_timer = global_timer + 1 * dt
 end
 
 function love.keyreleased(key)
